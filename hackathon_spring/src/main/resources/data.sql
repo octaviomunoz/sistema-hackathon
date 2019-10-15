d053bf2fd8b7faa7820e842777961fd7f854d7ff
octaviomunoz <ostavio09@gmail.com>
2019-10-15 17:10:46 -0300

    agregando data sql para pruebas

---
+++ hackathon_spring/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO hackathons (tema, descripcion, fechaRealizacion, fechaFinalizacionInscripcion, integrantesMaxEquipo, integrantesMinEquipo, color) VALUES
+('TEMA', 'DESCRIPCION', '12/11/2019', '08/11/2019', 1, 5, '#fff')
